89db85819c7d1d676a2d3a37becc1eef9a9286a0
Delwar Shawon <15070587+DoctorHammer@users.noreply.github.com>
2026-01-01 17:25:53 -0500

    Add officer memorial data to data.js
    Added officer data including details such as last name, officer name, date of death, tags, caption templates, and example tweet links for multiple officers.

---
+++ files/data.js
@@ -0,0 +1,212 @@
+// === PLACE ALL OFFICER DATA HERE ===
+        const data = [
+            {
+                "lastName": "riverajason",
+                "officer": "Detective Jason Rivera",
+                "dateOfDeath": "2022-01-21",
+                "tags": "jason rivera wilbert mora harlem domestic 32pct eow",
+                "captionTemplate": "Today we solemnly remember the {years}-year anniversary of the tragic loss of Detective Jason Rivera, who was killed in the line of duty while responding to a domestic call in Harlem. Four days later, his partner Detective Wilbert Mora succumbed to his injuries. We honor their bravery and sacrifice, and we vow to #NeverForget. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1881703356315627596"
+            },
+            {
+                "lastName": "mora",
+                "officer": "Detective Wilbert Mora",
+                "dateOfDeath": "2022-01-25",
+                "tags": "wilbert mora jason rivera harlem organ donor 32pct eow",
+                "captionTemplate": "On this day {years} years ago, Detective Wilbert Mora succumbed to wounds sustained in the line of duty alongside his partner, Detective Jason Rivera. A hero and organ donor whose gift saved lives, his legacy of service lives on. We will #NeverForget his sacrifice. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1883190981123588441"
+            },
+            {
+                "lastName": "simonsen",
+                "officer": "Detective Brian Simonsen",
+                "dateOfDeath": "2019-02-12",
+                "tags": "brian simonsen queens robbery friendly fire 102pct",
+                "captionTemplate": "Today marks {years} years since Detective Brian Simonsen was tragically killed while responding to a robbery in Queens. A 19-year veteran, his dedication and sacrifice will never be forgotten. #NeverForget #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1890071561824268389"
+            },
+            {
+                "lastName": "familia",
+                "officer": "Detective Miosotis Familia",
+                "dateOfDeath": "2017-07-05",
+                "tags": "miosotis familia assassination 46pct mother",
+                "captionTemplate": "{years} years ago today, Detective Miosotis Familia was assassinated while sitting in her command post in the Bronx. A devoted mother and officer, her courage and sacrifice remain in our hearts. We will #NeverForget.",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1941482402167107891"
+            },
+            {
+                "lastName": "morabito",
+                "officer": "Sergeant Joseph Morabito",
+                "dateOfDeath": "1971-08-26",
+                "tags": "joseph morabito belt parkway shooting",
+                "captionTemplate": "On this day {years} years ago, Sergeant Joseph Morabito was shot and killed in the line of duty on the Belt Parkway. His bravery and service to New York City will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1960357185143103890"
+            },
+            {
+                "lastName": "parker",
+                "officer": "Detective Myron Parker",
+                "dateOfDeath": "1987-08-20",
+                "tags": "myron parker intoxicated driver",
+                "captionTemplate": "{years} years ago today, Detective Myron Parker was struck and killed by an intoxicated driver while on duty. His sacrifice in protecting New Yorkers will always be remembered. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1958127397712367824"
+            },
+            {
+                "lastName": "pegues",
+                "officer": "Police Officer Thomas Pegues",
+                "dateOfDeath": "1974-08-19",
+                "tags": "thomas pegues queens car stop vietnam veteran",
+                "captionTemplate": "On this day {years} years ago, Police Officer Thomas Pegues, a Vietnam War veteran, was shot and killed during a car stop in Queens. His bravery and service will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1957785131831197717"
+            },
+            {
+                "lastName": "schimenti",
+                "officer": "Police Officer Thomas Schimenti",
+                "dateOfDeath": "1979-08-17",
+                "tags": "thomas schimenti transit bank robbery mts",
+                "captionTemplate": "{years} years ago, Transit Police Officer Thomas Schimenti was shot and killed while pursuing a bank robbery suspect. His courage and ultimate sacrifice will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1957204131015192728"
+            },
+            {
+                "lastName": "ryman",
+                "officer": "Officer Harry Ryman",
+                "dateOfDeath": "1980-08-14",
+                "tags": "harry ryman off duty car theft",
+                "captionTemplate": "On this day {years} years ago, Officer Harry Ryman was shot and killed off-duty while confronting suspects stealing his neighbor's car. His selfless act of bravery will always be remembered. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1956129959581196521"
+            },
+            {
+                "lastName": "venable",
+                "officer": "Police Officer Robert Venable",
+                "dateOfDeath": "1987-09-22",
+                "tags": "robert venable armed suspects",
+                "captionTemplate": "{years} years ago, Police Officer Robert Venable was shot and killed while attempting to apprehend two armed suspects. His courage and sacrifice will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1970262101894566309"
+            },
+            {
+                "lastName": "hopkins",
+                "officer": "Police Officer Melvin Hopkins",
+                "dateOfDeath": "1979-09-15",
+                "tags": "melvin hopkins robbery brooklyn",
+                "captionTemplate": "On this day {years} years ago, Police Officer Melvin Hopkins was shot and killed while responding to a robbery in Brooklyn. We honor his service and sacrifice. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1967628323246473544"
+            },
+            {
+                "lastName": "anderson",
+                "officer": "Police Officer Bruce Anderson",
+                "dateOfDeath": "1974-09-14",
+                "tags": "bruce anderson crash shooting",
+                "captionTemplate": "{years} years ago, Police Officer Bruce Anderson was killed while responding to a crash and officer-involved shooting. His legacy of service lives on. #NeverForget",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1967238647067156830"
+            },
+            {
+                "lastName": "fontanezdiaz",
+                "officer": "Police Officer Hector Fontanez-Diaz",
+                "dateOfDeath": "1991-09-12",
+                "tags": "hector fontanez diaz pursuit armed suspect",
+                "captionTemplate": "Today marks {years} years since Police Officer Hector Fontanez-Diaz was killed while pursuing an armed suspect. Despite returning fire, he succumbed to his injuries. His sacrifice will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1966498014870057129"
+            },
+            {
+                "lastName": "murray",
+                "officer": "Police Officer Brian J. Murray",
+                "dateOfDeath": "1976-09-11",
+                "tags": "brian murray bomb squad explosion",
+                "captionTemplate": "{years} years ago, Bomb Squad Officer Brian J. Murray was killed while deactivating a terrorist bomb. His extraordinary bravery saved countless lives. We will #NeverForget.",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1966251199772115308"
+            },
+            {
+                "lastName": "holder",
+                "officer": "Detective Randolph Holder",
+                "dateOfDeath": "2015-10-20",
+                "tags": "randolph holder psa5 pursuit east harlem",
+                "captionTemplate": "{years} years ago, Detective Randolph Holder was killed while pursuing an armed suspect in East Harlem. His heroism and sacrifice will never be forgotten. #NeverForget",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1980266204204859604"
+            },
+            {
+                "lastName": "tuozzolo",
+                "officer": "Sergeant Paul Tuozzolo",
+                "dateOfDeath": "2016-11-04",
+                "tags": "paul tuozzolo bronx armed suspect 43pct",
+                "captionTemplate": "Today marks {years} years since Sergeant Paul Tuozzolo was killed confronting an armed suspect in the Bronx. He gave his life protecting New Yorkers. We will never forget his sacrifice. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1985793831069970523"
+            },
+            {
+                "lastName": "ford",
+                "officer": "Detective Horace L. Ford",
+                "dateOfDeath": "1978-11-06",
+                "tags": "horace ford off duty bank robbery medal of honor",
+                "captionTemplate": "{years} years ago, off-duty Detective Horace L. Ford was killed confronting an armed bank robber. A Medal of Honor recipient, his heroism lives on. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1986500278150177209"
+            },
+            {
+                "lastName": "williamsguerzon",
+                "officer": "Detectives Keith Williams and Richard Guerzon",
+                "dateOfDeath": "1989-11-13",
+                "tags": "keith williams richard guerzon rikers prisoner transport",
+                "captionTemplate": "{years} years ago, Detectives Keith Williams and Richard Guerzon were shot and killed while transporting a prisoner. Their bravery and sacrifice will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1989115341923041366"
+            },
+            {
+                "lastName": "mckeon",
+                "officer": "Patrolman Francis McKeon",
+                "dateOfDeath": "1945-11-17",
+                "tags": "francis mckeon wwii navy veteran",
+                "captionTemplate": "{years} years ago, Patrolman Francis McKeon was killed on his first day back from serving in WWII with the U.S. Navy. We honor this hero's ultimate sacrifice. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1990525354767720589"
+            },
+            {
+                "lastName": "dunston",
+                "officer": "Officer James T. Dunston",
+                "dateOfDeath": "1980-11-20",
+                "tags": "james dunston psa5 robbery army veteran",
+                "captionTemplate": "{years} years ago, Officer James T. Dunston was shot and killed responding to a robbery. A U.S. Army veteran, father, and husband—his sacrifice will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1991476796756283398"
+            },
+            {
+                "lastName": "riverawilliam",
+                "officer": "Officer William Rivera",
+                "dateOfDeath": "2004-11-23",
+                "tags": "william rivera pursuit brooklyn",
+                "captionTemplate": "{years} years ago, Officer William Rivera succumbed to injuries sustained pursuing a suspect in Brooklyn. His dedication to keeping our city safe will always be remembered. #NeverForget",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1992671057472537061"
+            },
+            {
+                "lastName": "stewart",
+                "officer": "Detective Dillon Stewart",
+                "dateOfDeath": "2005-11-28",
+                "tags": "dillon stewart 70pct blood drive",
+                "captionTemplate": "{years} years ago, Detective Dillon Stewart was shot and killed in the line of duty. His legacy lives on through annual blood drives in his honor. We will #NeverForget.",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1994475180157030552"
+            },
+            {
+                "lastName": "ramosliu",
+                "officer": "Detectives Rafael Ramos and Wenjian Liu",
+                "dateOfDeath": "2014-12-20",
+                "tags": "rafael ramos wenjian liu ambush 84pct assassination",
+                "captionTemplate": "{years} years ago today, Detectives Rafael Ramos and Wenjian Liu were assassinated while sitting in their patrol car in Brooklyn. We honor their memory and vow to #NeverForget their sacrifice for New York City.",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/2002411231051764213"
+            },
+            {
+                "lastName": "enchautegui",
+                "officer": "Detective Daniel Enchautegui",
+                "dateOfDeath": "2005-12-10",
+                "tags": "daniel enchautegui off duty burglary medal of honor",
+                "captionTemplate": "{years} years ago, off-duty Detective Daniel Enchautegui was killed confronting burglars next door. A Medal of Honor recipient, his heroism will never be forgotten. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1998878889486221757"
+            },
+            {
+                "lastName": "forster",
+                "officer": "Police Officer Neil A. Forster",
+                "dateOfDeath": "1997-12-11",
+                "tags": "neil forster drunk driver purple shield",
+                "captionTemplate": "Today we honor Purple Shield recipient Police Officer Neil A. Forster, who {years} years ago succumbed to injuries after being struck by a drunk driver. His sacrifice will never be forgotten.",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1999198285672653036"
+            },
+            {
+                "lastName": "figoski",
+                "officer": "Detective Peter Figoski",
+                "dateOfDeath": "2011-12-12",
+                "tags": "peter figoski brooklyn home invasion 75pct",
+                "captionTemplate": "{years} years ago today, Detective Peter Figoski was shot and killed responding to a home invasion in Brooklyn. A devoted father and hero, we will never forget his ultimate sacrifice. #FidelisAdMortem",
+                "exampleTweetLink": "https://x.com/NYPDnews/status/1999464575972897081"
+            }
+        ];
+        // === END OF DATA ===
